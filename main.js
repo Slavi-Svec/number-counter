d9f17c9ee49879a18afe9d196fb28b00391d1e00
Slavi-Svec <slavi.svec@gmail.com>
2019-09-26 08:31:54 +1000

    Updates of JS features on counter

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ minusNum.addEventListener('click',downCounter);
 function upCounter() {
     count++;
     counter.innerHTML = count;
+    if(counter.innerHTML > '0') {
+     counter.style.color = '008F11'
+    }
+    else if(counter.innerHTML === '0'){
+     counter.style.color = 'white';
+    }
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration: 400,fill:'forwards'});
 }
 
 function downCounter() {
     count --;
     counter.innerHTML = count;
+    if(counter.innerHTML < '0') {
+     counter.style.color = 'red'
+    }
+    else if(counter.innerHTML === '0'){
+     counter.style.color = 'white';
+    }
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:400,fill:'forwards'});
 }
-
-
